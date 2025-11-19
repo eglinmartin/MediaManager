82fb7d436dafc9e3a1eb5599238e18be1fbba51e
Martin Eglin <eglin.martin@gmail.com>
2025-11-19 21:52:49 +0000

    Import from database in handler

--- src/handler.py
+++ src/handler.py
@@ -1,3 +1,4 @@
+import sqlite3
 from dataclasses import dataclass
 from datetime import datetime
 from typing import List
@@ -12,3 +13,27 @@ class Media:
     date: datetime
     media_type: str
     tags: List[str]
+
+
+def load_media_from_json(db_path: str) -> List[Media]:
+    """Load media list from a JSON file."""
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM Media")
+    data = cursor.fetchall()
+    column_names = [description[0] for description in cursor.description]
+
+    media_list = [
+        Media(
+            title=row[column_names.index('Title')],
+            director=row[column_names.index('Director')],
+            cast=row[column_names.index('Cast')].split(', '),
+            code=row[column_names.index('ID')],
+            date=datetime.fromisoformat(row[column_names.index('Date')]),
+            media_type=row[column_names.index('Type')],
+            tags=row[column_names.index('Tags')]
+        )
+        for row in data
+    ]
+    return media_list
